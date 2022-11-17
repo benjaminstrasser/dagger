4141c25c23614ce4c4a08fdb8c9c2aaee56c61f4
Benjamin Strasser <bp.strasser@gmail.com>
2022-11-17 11:41:19 +0100

    added running config for dagger inside nx
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

--- examples/nx-example/apps/dagger/src/main.ts
+++ examples/nx-example/apps/dagger/src/main.ts
@@ -1,1 +1,13 @@
-console.log(process.cwd());
+import Client, {connect} from "@dagger.io/dagger"
+
+
+connect(async (client:  Client) => {
+  const ctr = client
+    .container()
+    .from("node")
+    .withEntrypoint(["cowsay"])
+
+  const result = await ctr.exec(["Hello"]).stdout().contents()
+
+  console.log(result.contents)
+})
